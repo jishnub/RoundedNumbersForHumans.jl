a6e9844c7d1d638d3dce947883b00dbd79657d47
jishnub <jishnuonline@gmail.com>
2020-03-26 15:00:48 +0400

    round numbers to string, possibly converting to integer

--- src/RoundedNumbersForHumans.jl
+++ src/RoundedNumbersForHumans.jl
@@ -1,5 +1,70 @@
 module RoundedNumbersForHumans
 
-greet() = print("Hello World!")
+export RoundedNumber
+
+abstract type SuffixType end
+struct Scientific <: SuffixType end
+struct Common <: SuffixType end
+
+const suffixlistcommon = Dict(1=>"k", # kilo
+					2=>"M", # millions
+					3=>"B", # billions
+					4=>"T", # trillions
+					5=>"Qd", # quadrillions
+					6=>"Qn") # quintillions
+
+const suffixlistscientific = Dict(1=>"k", # kilo
+					2=>"M", # mega
+					3=>"G", # giga
+					4=>"T", # tera
+					5=>"P", # peta
+					6=>"E") # exa
+
+suffixlist(::Common) = suffixlistcommon
+suffixlist(::Scientific) = suffixlistscientific
+
+struct RoundedNumber{T}
+	n :: T
+	exp1000 :: Int
+end
+
+exponent1000(n::Integer) = floor(Int,log(1000,n))
+maxexp1000(::Common) = 6
+maxexp1000(::Scientific) = 6
+
+function RoundedNumber(n::Integer,suffixtype::SuffixType=Common())
+	exp1000 = exponent1000(n)
+	exp1000 = min(exp1000,maxexp1000(suffixtype))
+	RoundedNumber(n,exp1000)
+end
+
+prefixnumdigits(r::RoundedNumber) = prefixnumdigits(r.n,r.exp1000)
+function prefixnumdigits(n::Integer,exp1000::Integer)
+	ceil(Int,log10(n)) - 3*exp1000
+end
+
+function numberprefix(r::RoundedNumber;kwargs...)
+	numberprefix(r.n,r.exp1000,kwargs...)
+end
+function numberprefix(n::Integer,exp1000::Integer;sigdigits=3)
+	round(n/1000^exp1000,sigdigits=sigdigits)
+end
+
+function Base.string(r::RoundedNumber,suffixtype::SuffixType=Common();kwargs...)
+	if r.exp1000 != 0
+		suffix = suffixlist(suffixtype)[r.exp1000]
+	else
+		suffix = ""
+	end
+	prefixnum = numberprefix(r.n,r.exp1000;kwargs...)
+	# Not type-stable !
+	if isinteger(prefixnum)
+		prefixnum = Int(prefixnum)
+	end
+	prefix = string(prefixnum)
+	prefix*suffix
+end
+
+Base.show(io::IO,r::RoundedNumber) = print(io,string(r))
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,44 @@
 using RoundedNumbersForHumans
 using Test
+import RoundedNumbersForHumans: Scientific, Common
 
-@testset "RoundedNumbersForHumans.jl" begin
-    # Write your own tests here.
+@testset "string" begin
+    @test string(RoundedNumber(300)) == "300"
+    @test string(RoundedNumber(300),Common()) == "300"
+    @test string(RoundedNumber(300),Scientific()) == "300"
+    @test string(RoundedNumber(3_000)) == "3k"
+    @test string(RoundedNumber(3_000),Common()) == "3k"
+    @test string(RoundedNumber(3_000),Scientific()) == "3k"
+    @test string(RoundedNumber(3_000_000)) == "3M"
+    @test string(RoundedNumber(3_000_000),Common()) == "3M"
+    @test string(RoundedNumber(3_000_000),Scientific()) == "3M"
+    @test string(RoundedNumber(3_000_000_000)) == "3B"
+    @test string(RoundedNumber(3_000_000_000),Common()) == "3B"
+    @test string(RoundedNumber(3_000_000_000),Scientific()) == "3G"
+    @test string(RoundedNumber(3_000_000_000_000)) == "3T"
+    @test string(RoundedNumber(3_000_000_000_000),Common()) == "3T"
+    @test string(RoundedNumber(3_000_000_000_000),Scientific()) == "3T"
+    @test string(RoundedNumber(3_000_000_000_000_000)) == "3Qd"
+    @test string(RoundedNumber(3_000_000_000_000_000),Common()) == "3Qd"
+    @test string(RoundedNumber(3_000_000_000_000_000),Scientific()) == "3P"
+    @test string(RoundedNumber(3_000_000_000_000_000_000)) == "3Qn"
+    @test string(RoundedNumber(3_000_000_000_000_000_000),Common()) == "3Qn"
+    @test string(RoundedNumber(3_000_000_000_000_000_000),Scientific()) == "3E"
+
+    @testset "sigdigits" begin
+    	@testset "default" begin
+    	    @test string(RoundedNumber(3_100)) == "3.1k"
+		    @test string(RoundedNumber(3_150)) == "3.15k"
+		    @test string(RoundedNumber(3_153)) == "3.15k"
+		    @test string(RoundedNumber(31_532)) == "31.5k"
+    	end
+    	@testset "specified" begin
+		    @test string(RoundedNumber(31_532),sigdigits=1) == "30k"
+		    @test string(RoundedNumber(31_532),sigdigits=2) == "32k"
+		    @test string(RoundedNumber(31_532),sigdigits=3) == "31.5k"
+		    @test string(RoundedNumber(31_532),sigdigits=4) == "31.53k"
+		    @test string(RoundedNumber(31_532),sigdigits=5) == "31.532k"
+    	end
+    end
+
 end
